6135c79745cbc5aff4c68dd66eb51fa8ad5de3bc
cnmnui <50909979+cnmnui@users.noreply.github.com>
2019-05-22 18:23:11 +0800

    Add files via upload

---
+++ tencentJob.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import time
+import json
+from ..items import TencentjobItem
+
+
+class TencentjobSpider(scrapy.Spider):
+    name = 'tencentJob'
+    allowed_domains = ['tencent.com']
+    start_urls = ['https://careers.tencent.com/tencentcareer/api/post/Query?timestamp=1558510109687&countryId=&cityId='
+                  '&bgIds=&productId=&categoryId=&parentCategoryId=&attrId=&keyword=&pageIndex=1&pageSize=10'
+                  '&language=zh-cn&area=cn']
+    timestamp = round(time.time() * 1000)
+    pg_size = 10
+
+    # 获取职位总数
+    def parse(self, response):
+        res = json.loads(response.text)
+        job_num = res["Data"]["Count"]
+        print(job_num)
+        all_pg_num = int(job_num)//10 + 1
+        print(all_pg_num)
+        for pg_num in range(1, all_pg_num + 1):
+            list_url = 'https://careers.tencent.com/tencentcareer/api/post/Query?timestamp={0}&countryId=&cityId=' \
+                       '&bgIds=&productId=&categoryId=&parentCategoryId=&attrId=&keyword=&pageIndex={1}' \
+                       '&pageSize={2}&language=zh-cn&area=cn'.format(self.timestamp, pg_num, self.pg_size)
+            yield scrapy.Request(list_url, callback=self.parse_list_html)
+
+    def parse_list_html(self, response):
+        infos = json.loads(response.text)["Data"]["Posts"]
+        for info in infos:
+            PostID = info["PostId"]
+            job_url = 'https://careers.tencent.com/tencentcareer/api/post/ByPostId?timestamp={0}&postId={1}&' \
+                      'language=zh-cn'.format(self.timestamp, PostID)
+            yield scrapy.Request(job_url, callback=self.parse_jog_html)
+
+    def parse_jog_html(self, response):
+        item = TencentjobItem()
+        job_info = json.loads(response.text)["Data"]
+        item["RecruitPostName"] = job_info["RecruitPostName"]
+        item["BGName"] = job_info["BGName"]
+        item["LocationName"] = job_info["LocationName"]
+        item["CategoryName"] = job_info["CategoryName"]
+        item["LastUpdateTime"] = job_info["LastUpdateTime"]
+        item["Responsibility"] = job_info["Responsibility"]
+        item["Requirement"] = job_info["Requirement"]
+        yield item        
+        
+        
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
